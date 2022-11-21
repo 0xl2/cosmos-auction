9c42d0de3eebaea50055b5646242da5ef549e378
naruto0913 <abe.sinjiro7@gmail.com>
2022-11-22 07:12:13 +0900

    fix: minor fix

--- x/cosmosauction/types/genesis.go
+++ x/cosmosauction/types/genesis.go
@@ -4,9 +4,6 @@ import (
 // this line is used by starport scaffolding # genesis/types/import
 )
 
-// DefaultIndex is the default global index
-const DefaultIndex uint64 = 1
-
 // DefaultGenesis returns the default genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
@@ -18,7 +15,9 @@ func DefaultGenesis() *GenesisState {
 // Validate performs basic genesis state validation returning an error upon any
 // failure.
 func (gs GenesisState) Validate() error {
-	// this line is used by starport scaffolding # genesis/types/validate
+	if err := gs.Params.Validate(); err != nil {
+		return err
+	}
 
-	return gs.Params.Validate()
+	return nil
 }

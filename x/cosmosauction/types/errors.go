d88dc31715e925ba0d42942d61d0b3b66a408dd4
naruto0913 <abe.sinjiro7@gmail.com>
2022-11-22 07:55:47 +0900

    feat: add error cases

--- x/cosmosauction/types/errors.go
+++ x/cosmosauction/types/errors.go
@@ -10,3 +10,15 @@ import (
 var (
 	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
 )
+
+var (
+	AuctionInvalidName = sdkerrors.Register(ModuleName, 1101, "Auction name is invalid")
+	AuctionInvalidStart = sdkerrors.Register(ModuleName, 1102, "Auction start is invalid")
+)
+
+var (
+	BidInvalidAuction = sdkerrors.Register(ModuleName, 1201, "Auction id is invalid")
+	BidInvalidAmount = sdkerrors.Register(ModuleName, 1202, "Bid amount is invalid")
+	BidLowAmount = sdkerrors.Register(ModuleName, 1203, "Bid amount is under")
+	BidDuplciateBidder = sdkerrors.Register(ModuleName, 1203, "Bidder is duplicated")
+)
